fe4eaf5a14db9bb95c8d64f9eddd47eff36c0a50
Henry Van Hove <106410970+henryavh@users.noreply.github.com>
2023-11-09 16:14:41 -0500

    more updates to savetodoc

--- savetodoc/savetodoc.py
+++ savetodoc/savetodoc.py
@@ -1,27 +1,44 @@
-from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
 from reportlab.platypus import Paragraph
 from reportlab.lib.units import inch
 
 def save_to_pdf(file_path, prompt_container):
-    canvas_width, canvas_height = letter
+    page_width, page_height = letter
     c = canvas.Canvas(file_path, pagesize=letter)
-    style = getSampleStyleSheet()['Normal']
-    style.fontName = 'Helvetica'
-    style.fontSize = 12
-    style.leading = 15  # Line spacing
+    styles = getSampleStyleSheet()
+    title_style = styles['Title']
 
-    # Center the document title
-    doc_title = prompt_container.docName
-    title_width = c.stringWidth(doc_title, style.fontName, style.fontSize)
-    centered_title_x = (canvas_width - title_width) / 2
-    c.drawString(centered_title_x, canvas_height - 50, doc_title)
+    c.setFillColor(colors.darkblue)
+    title = prompt_container.docName
+    title_width = c.stringWidth(title, title_style.fontName, title_style.fontSize)
+    c.setFont(title_style.fontName, title_style.fontSize)
+    c.drawString((page_width - title_width) / 2, page_height - 70, title)
+
+    prompt_text_style = styles['Title'] 
+    prompt_text_style.alignment = 1  
+    prompt_response_style = styles['BodyText']
+    prompt_response_style.textColor = colors.black
+
+    y_position = page_height - 150
 
     for prompt in prompt_container.customPromptList:
-        paragraph = Paragraph(prompt.prompttext, style)
-        paragraph_width, paragraph_height = paragraph.wrap(canvas_width - 2 * inch, canvas_height)  # Wrap the paragraph
-        centered_paragraph_x = (canvas_width - paragraph_width) / 2
-        paragraph.drawOn(c, centered_paragraph_x, prompt.y - paragraph_height)
+        prompttext = prompt.prompttext
+        p_text = Paragraph(prompttext, prompt_text_style)
+        text_width, text_height = p_text.wrap(page_width, y_position)
+        p_text.drawOn(c, (page_width - text_width) / 2, y_position - text_height)
+        y_position -= text_height
+
+        promptresponse = prompt.promptresponse
+        p_response = Paragraph(promptresponse, prompt_response_style)
+        response_width, response_height = p_response.wrap(page_width - 2 * inch, y_position)
+        p_response.drawOn(c, inch, y_position - response_height)
+        y_position -= (response_height + 20)
+
+        if y_position > 20:
+            c.line(50, y_position, page_width - 50, y_position)
+            y_position -= 20
 
     c.save()
